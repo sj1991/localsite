908c1f435c306524a5bc507c9f87ef64afb2effe
Colin Bookman <cobookman@gmail.com>
2014-02-20 00:05:35 -0500

    Forgot to delete commented lines

--- localsite/app/models/organization.js
+++ localsite/app/models/organization.js
@@ -10,20 +10,6 @@ function(backbone) {
     },
     url : function() {
       return '/app/api/organizations/index.php?id=' + this.id;
-    },
-    parse : function(res, options) {
-      // //If a collection is calling this method, we need to grab the refernce of user
-      // if(typeof this.user === 'undefined' && typeof options.user !== 'undefined') {
-      //   this.user = options.user;
-      // }
-
-      // //Add property which shows if user belongs to the org
-      // if(this.user.get('organizations').get(res.id)) {
-      //   res.joined = true;
-      // } else {
-      //   res.joined = false;
-      // }
-      return res;
     }
   });
   return OrganizationModel;
