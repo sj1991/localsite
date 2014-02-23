87d0ad748b5f580535d42a9ab4a89573756578c4
Colin Bookman <cobookman@gmail.com>
2014-02-23 14:18:45 -0500

    added example output
    added an example response from the API for debugging work w/front-end. Also to help backend developers in there coding adventure, as now they've got something to work with.

--- api/controllers/Organization.php
+++ api/controllers/Organization.php
@@ -1,9 +1,13 @@
 <?php
 
 class Organization {
-
   public static function get($id) {
-    echo "Getting organization with Id: $id";
+      $exampleOrganization = file_get_contents('./exampleOutput/organization.json', FILE_USE_INCLUDE_PATH);
+      echo $exampleOrganization;
+  }
+  public static function getAll() {
+      $exampleOrganizationList = file_get_contents('./exampleOutput/organizations.json', FILE_USE_INCLUDE_PATH);
+      echo $exampleOrganizationList;
   }
   public static function join($id) {
     echo "Joining organization with Id: $id";
--- api/controllers/User.php
+++ api/controllers/User.php
@@ -2,7 +2,8 @@
 
 class User {
   public static function get($id) {
-    echo "Getting User: $id"; 
+    $exampleUser = file_get_contents('./exampleOutput/user.json', FILE_USE_INCLUDE_PATH);
+    echo $exampleUser;
   }
   public static function permissions($id) {
     echo "Changing permissions for: $id";
