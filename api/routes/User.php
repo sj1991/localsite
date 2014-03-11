51ac3a8be8e133b859d9085fc663b8703587924a
EviticsDev <evitics@evitics-VirtualBox.(none)>
2014-03-10 20:04:59 -0400

    Modified API calls

--- api/routes/User.php
+++ api/routes/User.php
@@ -2,9 +2,21 @@
 
 class User {
   public static function get($id) {
-    $exampleUser = file_get_contents('./exampleOutput/user.json', FILE_USE_INCLUDE_PATH);
+
+try {
+	$query = sprintf("SELECT username, name, email, text, organizations FROM users WHERE orgId='%d'", intval($id));	
+	$result = mysql_query($query);
+	while($row=mysql_fetch_assoc($result))
+	{
+		$org[] = $row;
+	} 
+	$struct = array("Org"=>$org);
+	echo json_encode($struct);
+    
+  /*  $exampleUser = file_get_contents('./exampleOutput/user.json', FILE_USE_INCLUDE_PATH);
     echo $exampleUser;
-  }
+  */
+}
   public static function permissions($id) {
     echo "Changing permissions for: $id";
   }
