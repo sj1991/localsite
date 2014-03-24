8530ab6fe3f02acadbe923b8deff5dbb33c26b4e
Colin Bookman <cobookman@gmail.com>
2014-03-23 23:09:09 -0400

    Removed
    Not sure why it was there, but its un-needed and shouldn't be global

--- api/index.php
+++ api/index.php
@@ -16,8 +16,6 @@ spl_autoload_register(function ($class_name) {
         require_once($file);
     }
 });
-//Load config into global namespace
-$_CONFIG = require("./config.php");
 //Binds our routes
 require "./router.php";
 
