fd26be9fc70946b071899b946e506b29e75c4b82
EviticsDev <evitics@evitics-VirtualBox.(none)>
2014-03-27 15:54:49 -0400

    Updated Org APIs

--- api/routes/Organization.php
+++ api/routes/Organization.php
@@ -1,43 +1,42 @@
 <?php
+
+require("./library/DB.php");
+require(config.php);
+
+$db = new DB("jacketpages");
+
 class Organization {
   public static function get($id) {
     try {
-	$query = sprintf("SELECT orgId,name,description,logo_path FROM organizations WHERE orgId='%d'", intval($id));	
-	$result = mysql_query($query);
-	while($row=mysql_fetch_assoc($result))
+	$query = "SELECT orgId,name,description,logo_path FROM 'organizations' WHERE 'orgId' =:orgId";	
+	$result = $db->fetchAll($query, array("orgId"=>$id));
+	foreach($result as $value)
 	{
-		$org[] = $row;
-		$org[]->logo_path = "http://jacketpages.gatech.edu" . $org[]->logo_path; 		} 
-	$struct = array("Org"=>$org);
-	echo json_encode($struct);
-    
-      /*$organizations = json_decode(file_get_contents('./exampleOutput/organizations.json', FILE_USE_INCLUDE_PATH));
-      foreach($organizations as $organization) {
-        if($organization->id == $id) {
-          echo json_encode($organization);
-          return;
-        } */
-      }
+	  $result->logo_path = "http:\/\/jacketpages.gatech.edu". $result->logo_path;
+	}	
+	echo json_encode($result);
+        }
       echo '{"error" : "no organization with id: '.$id.'"}';
     } catch(Exception $e) {
       echo '{"error" : "Something broke :("}';
     }
   }
   public static function getAll() {
-	$result = mysql_query('SELECT orgId,name,description,logo_path FROM organizations ');
-	while($row=mysql_fetch_assoc($result))
+	try
 	{
-		$org[] = $row;
-		$org[]->logo_path = "http://jacketpages.gatech.edu" . $org[]->logo_path; 		} 
-	$struct = array("Orgs"=>$org);
-	echo json_encode($struct);
-    
-/* 
-	$exampleOrganizationList = file_get_contents('./exampleOutput/organizations.json', FILE_USE_INCLUDE_PATH);
-    echo $exampleOrganizationList;
-    */
-  }
-  public static function join($id) {
+	$query = "SELECT orgId,name,description,logo_path FROM 'organizations'";
+	$result = $ddb->fetchAll($query, array());
+	echo json_encode($result);
+    	foreach($result as $value)
+	{
+	  $result->logo_path = "http:\/\/jacketpages.gatech.edu". $result->logo_path;
+	}	
+	}
+catch(Exception $e) {
+      echo '{"error" : "Something broke :("}';
+    }
+} 
+ public static function join($id) {
     echo "Joining organization with Id: $id";
   }
   
