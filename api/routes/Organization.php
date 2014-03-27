31009d22e48128f1a00c1db5bfcb23ad4596a476
EviticsDev <evitics@evitics-VirtualBox.(none)>
2014-03-27 18:01:03 -0400

    Organization API added

--- api/routes/Organization.php
+++ api/routes/Organization.php
@@ -1,45 +1,50 @@
 <?php
 
 require("./library/DB.php");
-require(config.php);
-
-$db = new DB("jacketpages");
+require("./config.php");
 
 class Organization {
-  public static function get($id) {
-    try {
-	$query = "SELECT orgId,name,description,logo_path FROM 'organizations' WHERE 'orgId' =:orgId";	
-	$result = $db->fetchAll($query, array("orgId"=>$id));
-	foreach($result as $value)
+  public static function get($id) 
+  {
+	$db = new DB("jacketpages");
+	try 
 	{
-	  $result->logo_path = "http:\/\/jacketpages.gatech.edu". $result->logo_path;
-	}	
-	echo json_encode($result);
+		$query = "SELECT `orgId`, `name`, `description`, `logo_path` FROM `organizations` WHERE `orgId` =:orgId";	
+		$result = $db->fetchAll($query, array("orgId"=>$id));
+		foreach($result as &$value)
+		{
+			$value["logo_path"] = "http://jacketpages.gatech.edu". $value["logo_path"];
+		} 	
+		echo json_encode($result);
         }
-      echo '{"error" : "no organization with id: '.$id.'"}';
-    } catch(Exception $e) {
-      echo '{"error" : "Something broke :("}';
-    }
+	catch(Exception $e) 
+	{
+      		echo '{"error" : "Something broke :("}';
+    	}
   }
-  public static function getAll() {
+  public static function getAll() 
+  {
+	$db = new DB("jacketpages");
 	try
 	{
-	$query = "SELECT orgId,name,description,logo_path FROM 'organizations'";
-	$result = $ddb->fetchAll($query, array());
-	echo json_encode($result);
-    	foreach($result as $value)
-	{
-	  $result->logo_path = "http:\/\/jacketpages.gatech.edu". $result->logo_path;
-	}	
+		$query = "SELECT `orgId`, `name`, `description`, `logo_path` FROM `organizations`";
+		$result = $db->fetchAll($query, array());
+		foreach($result as &$value)
+		{
+			$value["logo_path"] = "http://jacketpages.gatech.edu". $value["logo_path"];
+		} 	
+		echo json_encode($result);	
 	}
-catch(Exception $e) {
-      echo '{"error" : "Something broke :("}';
-    }
-} 
- public static function join($id) {
+	catch(Exception $e) 
+	{
+      		echo '{"error" : "Something broke :("}';
+    	}
+  } 
+ 
+public static function join($id) 
+  {
     echo "Joining organization with Id: $id";
   }
-  
 }
 
 
