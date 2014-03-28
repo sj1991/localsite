eb93fb2ef8f91f88df3960f7b9e072d5ed82d0ac
EviticsDev <evitics@evitics-VirtualBox.(none)>
2014-03-27 20:12:30 -0400

    Working on User APi calls

--- api/routes/User.php
+++ api/routes/User.php
@@ -1,19 +1,29 @@
 <?php
-require("./library/DB.php");
-require("./library/GTED.php");
-$gted = new GTED();
-$db = new DB("evitics");
-$db2 = new DB("jacketpages");
+require_once("./library/DB.php");
+require_once("./library/GTED.php");
+require_once("./config.php");
+
 class User {
-  public static function get($id) {
-	try {
-	$query = "SELECT * FROM 'user' LEFT OUTER JOIN 'organizations' ON user.orgId = organizations.orgId WHERE 'userId'=:userId";	
-	$result = $db->fetchAll($query, array("userId"=>$id));
-	echo json_encode($result);
-    }
-  public static function permissions($id) {
+  public static function get($id) 
+  {
+	$db = new DB("evitics");
+	$gted = new GTED();	
+	try 
+	{
+		$query = "SELECT `orgId`, `writePerm`, `isPending` FROM `user` WHERE `userId`=:userId";	
+		$result = $db->fetchAll($query, array("userId"=>$id));
+		//echo var_dump($result);
+		echo var_dump($gted->queryGTUsername($id));		
+		//echo json_encode($result);
+    	}
+  	catch(Exception $e) 
+	{
+      		echo '{"error" : "Something broke :("}';
+    	}
+  }
+  public static function permissions($id) 
+  {
     echo "Changing permissions for: $id";
   }
 }
-
 ?>
