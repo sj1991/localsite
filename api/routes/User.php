c38d49b6cdbab00ad54dc446e6a367b514621c15
EviticsDev <evitics@evitics-VirtualBox.(none)>
2014-03-27 22:57:47 -0400

    Added the API call for suer

--- api/routes/User.php
+++ api/routes/User.php
@@ -7,15 +7,39 @@ class User {
   public static function get($id) 
   {
 	$db = new DB("evitics");
+	$dbo = new Db("jacketpages");
 	$gted = new GTED();	
 	try 
 	{
+		$resutl = array();
+		
+		//Getting the User info from GTED		
+		$userinfo = $gted->queryGTUsername($id);
+		$result["email"] = $userinfo["mail"][0];
+		$result["firstname"]= $userinfo["givenname"][0];
+		$result["lastnname"] = $userinfo["sn"][0];	
+		
 		$query = "SELECT `orgId`, `writePerm`, `isPending` FROM `user` WHERE `userId`=:userId";	
-		$result = $db->fetchAll($query, array("userId"=>$id));
-		//echo var_dump($result);
-		echo var_dump($gted->queryGTUsername($id));		
-		//echo json_encode($result);
-    	}
+		$res = $db->fetchAll($query, array("userId"=>$id));
+		$query2 = "SELECT `orgId`,`name`, `description`, `website`, `logo_path`, `org_email`, `phone_number` FROM `organizations` WHERE `orgId`=:orgId";	
+		$i=0;
+		foreach($res as $value)
+			{
+
+			$result["organizations"][$i]["orgId"] = $value["orgId"];			
+			$result["organizations"][$i]["writePerm"] = $value["writePerm"];
+			$result["organizations"][$i]["isPending"] = $value["isPending"];		
+			$res2 = $dbo->fetchAll($query2, array("orgId"=>intval($value["orgId"])));
+			$result["organizations"][$i]["name"] = $res2[0]["name"];
+			$result["organizations"][$i]["website"] = $res2[0]["website"];
+			$result["organizations"][$i]["description"] = $res2[0]["description"];	
+			$result["organizations"][$i]["email"] = $res2[0]["org_email"];			
+			$result["organizations"][$i]["phone"] = $res2[0]["phone_number"];
+			$result["organizations"][$i]["logo"] = "http://jacketpages.gatech.edu". $res2[0]["logo_path"];							
+			$i = $i+1;
+		}
+	echo json_encode($result);	
+	} 
   	catch(Exception $e) 
 	{
       		echo '{"error" : "Something broke :("}';
